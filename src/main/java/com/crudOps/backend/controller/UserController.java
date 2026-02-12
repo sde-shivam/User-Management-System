3829123166f7ed23af593cb5e608af6ab741b6cf
Shivam Singh <shivamasingh.tcsc@gmail.com>
2026-02-12 23:01:43 +0530

    integrate frontend UI with backend API and fix React setup

--- src/main/java/com/crudOps/backend/controller/UserController.java
+++ src/main/java/com/crudOps/backend/controller/UserController.java
@@ -3,14 +3,12 @@ package com.crudOps.backend.controller;
 import com.crudOps.backend.model.User;
 import com.crudOps.backend.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin("http://localhost:5173")
 public class UserController {
 
    @Autowired
