9b37d6e0e2ff972637073c5e7df14f0b673b355b
Shivam Singh <shivamasingh.tcsc@gmail.com>
2026-02-13 11:20:44 +0530

    implement ViewUser routing, connect View button, add AddUser form, and UI alignment fixes

--- src/main/java/com/crudOps/backend/controller/UserController.java
+++ src/main/java/com/crudOps/backend/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.crudOps.backend.controller;
 
+import com.crudOps.backend.exception.UserNotFoundException;
 import com.crudOps.backend.model.User;
 import com.crudOps.backend.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,12 +16,38 @@ public class UserController {
    private UserRepository userRepository;
 
    @PostMapping("/user")
-   User newUser(@RequestBody User newUser){
-       return userRepository.save(newUser);
+   User newUser(@RequestBody User newUser) {
+      return userRepository.save(newUser);
    }
 
    @GetMapping("/users")
-   List<User> getAllUsers(){
+   List<User> getAllUsers() {
       return userRepository.findAll();
+   }
+
+
+   @GetMapping("/user/{id}")
+   User getUserById(@PathVariable Long id) {
+      return userRepository.findById(id)
+              .orElseThrow(() -> new UserNotFoundException(id));
+   }
+
+   @PutMapping("/user/{id}")
+   User updateUser(@RequestBody User newUser,@PathVariable Long id){
+      return userRepository.findById(id)
+              .map(user->{
+                 user.setUsername(newUser.getUsername());
+                 user.setName(newUser.getName());
+                 user.setEmail(newUser.getEmail());
+                 return userRepository.save(user);
+              }).orElseThrow(() -> new UserNotFoundException(id));
+   }
+   @DeleteMapping("/user/{id}")
+   String deleteUser(@PathVariable Long id){
+     if(!userRepository.existsById(id)){
+        throw new UserNotFoundException(id);
+     }
+     userRepository.deleteById(id);
+     return "User with id "+id+" has been deleted successfully.";
    }
 }
